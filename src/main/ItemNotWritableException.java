f1f168b6b030d1c2f4c6f4da34369b9e34733eff
RobbeVanslambrouck1 <robbe.vanslambrouck1@student.kuleuven.be>
2022-04-07 15:50:16 +0200

    File param changed to item

--- src/main/ItemNotWritableException.java
+++ src/main/ItemNotWritableException.java
@@ -1,7 +1,7 @@
 import be.kuleuven.cs.som.annotate.*;
 
 /**
- * A class for signaling illegal attempts to change a file.
+ * A class for signaling illegal attempts to change a item.
  * 
  * @author 	Tommy Messelis
  * @version	2.2
@@ -14,30 +14,30 @@ public class ItemNotWritableException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * Variable referencing the file to which change was denied.
+	 * Variable referencing the item to which change was denied.
 	 */
-	private final File file;
+	private final namedWritableItem item;
 
 	/**
-	 * Initialize this new file not writable exception involving the
-	 * given file.
+	 * Initialize this new item not writable exception involving the
+	 * given item.
 	 * 
-	 * @param	file
-	 * 			The file for the new file not writable exception.
-	 * @post	The file involved in the new file not writable exception
-	 * 			is set to the given file.
-	 * 			| new.getFile() == file
+	 * @param	item
+	 * 			The item for the new item not writable exception.
+	 * @post	The item involved in the new item not writable exception
+	 * 			is set to the given item.
+	 * 			| new.getItem() == item
 	 */
-	public ItemNotWritableException(File file) {
-		this.file = file;
+	public ItemNotWritableException(namedWritableItem item) {
+		this.item = item;
 	}
 	
 	/**
 	 * Return the file involved in this file not writable exception.
 	 */
 	@Basic @Immutable
-	public File getFile() {
-		return file;
+	public namedWritableItem getItem() {
+		return item;
 	}
 	
 }
