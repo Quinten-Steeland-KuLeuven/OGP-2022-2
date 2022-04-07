f75cec72eea3ad79fe117bbf60ca5699c2ba2cee
RobbeVanslambrouck1 <robbe.vanslambrouck1@student.kuleuven.be>
2022-04-07 15:51:55 +0200

    moved modificationTime from namedItem to namedWritableItem

--- src/main/namedItem.java
+++ src/main/namedItem.java
@@ -18,12 +18,6 @@ public abstract class namedItem {
      */
     private final Date creationTime = new Date();
 
-    /**
-     * Variable referencing the time of the last modification,
-     * possibly null.
-     */
-    private Date modificationTime = null;
-
 
     public namedItem (String name) {
         setName(name);
@@ -52,7 +46,7 @@ public abstract class namedItem {
      */
     @Raw
     @Model
-    private void setName(String name) {
+    protected void setName(String name) {
         if (isValidName(name)) {
             this.name = name;
         } else {
@@ -87,85 +81,6 @@ public abstract class namedItem {
     private static String getDefaultName() {
         return "new_item";
     }
-
-
-    /**
-     * Change the name of this file to the given name.
-     *
-     * @param	name
-     * 			The new name for this file.
-     * @effect  The name of this file is set to the given name,
-     * 			if this is a valid name and the file is writable,
-     * 			otherwise there is no change.
-     * 			| if (isValidName(name) && isWritable())
-     *          | then setName(name)
-     * @effect  If the name is valid and the file is writable, the modification time
-     * 			of this file is updated.
-     *          | if (isValidName(name) && isWritable())
-     *          | then setModificationTime()
-     * @throws ItemNotWritableException(this)
-     *          This file is not writable
-     *          | ! isWritable()
-     */
-    public void changeName(String name) throws ItemNotWritableException {
-        if (isValidName(name)){
-                setName(name);
-                setModificationTime();
-            }
-    }
-
-    /**********************************************************
-     * modificationTime
-     **********************************************************/
-
-
-    /**
-     * Return the time at which this file was last modified, that is
-     * at which the name or size was last changed. If this file has
-     * not yet been modified after construction, null is returned.
-     */
-    @Raw @Basic
-    public Date getModificationTime() {
-        return modificationTime;
-    }
-
-    /**
-     * Check whether this file can have the given date as modification time.
-     *
-     * @param	date
-     * 			The date to check.
-     * @return 	True if and only if the given date is either not effective
-     * 			or if the given date lies between the creation time and the
-     * 			current time.
-     *         | result == (date == null) ||
-     *         | ( (date.getTime() >= getCreationTime().getTime()) &&
-     *         |   (date.getTime() <= System.currentTimeMillis())     )
-     */
-    @Raw
-    public boolean canHaveAsModificationTime(Date date) {
-        return (date == null) ||
-                ( (date.getTime() >= getCreationTime().getTime()) &&
-                        (date.getTime() <= System.currentTimeMillis()) );
-    }
-
-    /**
-     * Set the modification time of this file to the current time.
-     *
-     * @post   The new modification time is effective.
-     *         | new.getModificationTime() != null
-     * @post   The new modification time lies between the system
-     *         time at the beginning of this method execution and
-     *         the system time at the end of method execution.
-     *         | (new.getModificationTime().getTime() >=
-     *         |                    System.currentTimeMillis()) &&
-     *         | (new.getModificationTime().getTime() <=
-     *         |                    (new System).currentTimeMillis())
-     */
-    @Model
-    private void setModificationTime() {
-        modificationTime = new Date();
-    }
-
 
     /**********************************************************
      * creationTime
--- src/main/namedWritableItem.java
+++ src/main/namedWritableItem.java
@@ -1,10 +1,14 @@
 import be.kuleuven.cs.som.annotate.Basic;
+import be.kuleuven.cs.som.annotate.Model;
 import be.kuleuven.cs.som.annotate.Raw;
+
+import java.util.Date;
 
 public class namedWritableItem extends namedItem {
 
     public namedWritableItem (String name, boolean isWritable) {
         super(name);
+        setWritable(isWritable);
     }
 
     /**
@@ -33,6 +37,89 @@ public class namedWritableItem extends namedItem {
     @Raw
     public void setWritable(boolean isWritable) {
         this.isWritable = isWritable;
+    }
+
+    /**
+     * Variable referencing the time of the last modification,
+     * possibly null.
+     */
+    private Date modificationTime = null;
+
+    /**
+     * Change the name of this file to the given name.
+     *
+     * @param	name
+     * 			The new name for this file.
+     * @effect  The name of this file is set to the given name,
+     * 			if this is a valid name and the file is writable,
+     * 			otherwise there is no change.
+     * 			| if (isValidName(name) && isWritable())
+     *          | then setName(name)
+     * @effect  If the name is valid and the file is writable, the modification time
+     * 			of this file is updated.
+     *          | if (isValidName(name) && isWritable())
+     *          | then setModificationTime()
+     * @throws ItemNotWritableException(this)
+     *          This file is not writable
+     *          | ! isWritable()
+     */
+    public void changeName(String name) throws ItemNotWritableException {
+        if(!isWritable()) throw new ItemNotWritableException(this);
+        if (!isValidName(name)) return;
+        setName(name);
+        setModificationTime();
+    }
+
+    /**********************************************************
+     * modificationTime
+     **********************************************************/
+
+
+    /**
+     * Return the time at which this file was last modified, that is
+     * at which the name or size was last changed. If this file has
+     * not yet been modified after construction, null is returned.
+     */
+    @Raw @Basic
+    public Date getModificationTime() {
+        return modificationTime;
+    }
+
+    /**
+     * Check whether this file can have the given date as modification time.
+     *
+     * @param	date
+     * 			The date to check.
+     * @return 	True if and only if the given date is either not effective
+     * 			or if the given date lies between the creation time and the
+     * 			current time.
+     *         | result == (date == null) ||
+     *         | ( (date.getTime() >= getCreationTime().getTime()) &&
+     *         |   (date.getTime() <= System.currentTimeMillis())     )
+     */
+    @Raw
+    public boolean canHaveAsModificationTime(Date date) {
+        return (date == null) ||
+                ( (date.getTime() >= getCreationTime().getTime()) &&
+                        (date.getTime() <= System.currentTimeMillis()) );
+    }
+
+    /**
+     * Set the modification time of this file to the current time.
+     *
+     * @post   The new modification time is effective.
+     *         | new.getModificationTime() != null
+     * @post   The new modification time lies between the system
+     *         time at the beginning of this method execution and
+     *         the system time at the end of method execution.
+     *         | (new.getModificationTime().getTime() >=
+     *         |                    System.currentTimeMillis()) &&
+     *         | (new.getModificationTime().getTime() <=
+     *         |                    (new System).currentTimeMillis())
+     */
+    @Model
+    protected void setModificationTime() {
+        modificationTime = new Date();
     }
 
 }
