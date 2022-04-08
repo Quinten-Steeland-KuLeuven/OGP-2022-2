e33b50e1c2999ccb8cefea0123526cceaaff18a3
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-04-08 14:45:12 +0200

    init dirTest (todos)

---
+++ src/test/DirectoryTest.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.*;
+import java.util.Date;
+
+import org.junit.*;
+
+/**
+ * A JUnit test class for testing the public methods of the Directory Class
+ * @author Robbe Vanslambrouck
+ *
+ */
+public class DirectoryTest {
+
+    // name
+    // times (mod and creation)
+    // isWritable
+    // isRoot
+    // using arrayList
+    // no items with the same name (don't ignore capital letters)
+    // no loops
+    // contents is ordered (also after name change) (ignore capital letters)
+    // contents contains no null ref
+    // contents contains no refs to same item
+
+    // getNbItems
+    // getItemAt
+    // getItem
+    // containsDiskItemWithName
+    // getIndexOf
+    // hasAsItem
+
+
+}
